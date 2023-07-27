d0fe898b5e6d9164fd098302e7c76b7207509e7e
Pablo Rodríguez <prodrigu75@gmail.com>
2023-07-27 03:21:00 -0400

    Added dashboard pages and layout

---
+++ src/app/dashboard/rest-todos/page.tsx
@@ -0,0 +1,9 @@
+import { WidgetItem } from 'AdminTodos/common/components';
+
+export default function RestTodosPage() {
+  return (
+    <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
+      <WidgetItem />
+    </div>
+  );
+}
